53c7b8786932bc6a8a6b86b167704bae5545b058
Saddam Husain <sadamyggakpernahp@gmail.com>
2021-12-02 22:45:17 +0700

    refactor: Leveling up button component with adding element, theme, size, icon props and more logic to make button more reusable

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,11 +1,83 @@
 import styles from './Button.module.scss';
 
-function Button({ text, input }) {
-  return (
-    <button {...input} className={styles.button}>
-      {text}
-    </button>
-  );
+function Button({ element, theme, size, text, input, icon = null }) {
+  if (element === 'button' && theme === 'primary') {
+    return (
+      <button {...input} className={`${styles.buttonPrimary} ${styles[size]}`}>
+        {icon}
+        <span>{text}</span>
+      </button>
+    );
+  }
+
+  if (element === 'button' && theme === 'secondary-color') {
+    return (
+      <button
+        {...input}
+        className={`${styles.buttonSecondaryColor} ${styles[size]}`}
+      >
+        {icon}
+        <span>{text}</span>
+      </button>
+    );
+  }
+
+  if (element === 'button' && theme === 'secondary-gray') {
+    return (
+      <button
+        {...input}
+        className={`${styles.buttonSecondaryGray} ${styles[size]}`}
+      >
+        {icon}
+        <span>{text}</span>
+      </button>
+    );
+  }
+
+  if (element === 'link' && theme === 'primary') {
+    return (
+      <a {...input} className={`${styles.linkPrimary} ${styles[size]}`}>
+        {icon}
+        <span>{text}</span>
+      </a>
+    );
+  }
+
+  if (element === 'link' && theme === 'secondary-color') {
+    return (
+      <a {...input} className={`${styles.linkSecondaryColor} ${styles[size]}`}>
+        {icon}
+        <span>{text}</span>
+      </a>
+    );
+  }
+
+  if (element === 'link' && theme === 'secondary-gray') {
+    return (
+      <a {...input} className={`${styles.linkSecondaryGray} ${styles[size]}`}>
+        {icon}
+        <span>{text}</span>
+      </a>
+    );
+  }
+
+  if (element === 'link' && theme === 'color') {
+    return (
+      <a {...input} className={`${styles.linkColor} ${styles[size]}`}>
+        {icon}
+        <span>{text}</span>
+      </a>
+    );
+  }
+
+  if (element === 'link' && theme === 'gray') {
+    return (
+      <a {...input} className={`${styles.linkGray} ${styles[size]}`}>
+        {icon}
+        <span>{text}</span>
+      </a>
+    );
+  }
 }
 
 export default Button;
