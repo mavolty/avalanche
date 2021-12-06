a2436353d256485b10705fd9996113999d4dc998
Saddam Husain <sadamyggakpernahp@gmail.com>
2021-12-06 14:18:07 +0700

    refactor: Add new logic if there is label in input component

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -3,9 +3,11 @@ import styles from './Input.module.scss';
 function Input({ label, input }) {
   return (
     <div className={styles.field}>
-      <label htmlFor={input.id} className={styles.label}>
-        {label}
-      </label>
+      {label && (
+        <label htmlFor={input.id} className={styles.label}>
+          {label}
+        </label>
+      )}
       <input className={styles.input} {...input} />
     </div>
   );
