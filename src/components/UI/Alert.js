4034523029b0b8be8c9e7bfb55be2d082354e872
Saddam Husain <sadamyggakpernahp@gmail.com>
2021-12-07 23:15:17 +0700

    feat: Add alert component

---
+++ src/components/UI/Alert.js
@@ -0,0 +1,17 @@
+import styles from './Alert.module.scss';
+import Icon from './Icons/Icon';
+
+function Alert({ type, message }) {
+  const containerClasses = `${styles.container} ${styles[type]}`;
+  console.log(type, message);
+
+  return (
+    <div className={containerClasses}>
+      {type === 'error' && <Icon name="exclamation-circle" color="error" />}
+      {type === 'success' && <Icon name="tick-circle" color="success" />}
+      <p className={styles.message}>{message}</p>
+    </div>
+  );
+}
+
+export default Alert;
