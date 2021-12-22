64a918233d7f32a743cbe416a45dc709f1697f67
Saddam Husain <sadamyggakpernahp@gmail.com>
2021-12-22 19:47:21 +0700

    refactor: Change anchor element to Link react router in button component and also add link white and black"

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,6 +1,27 @@
 import styles from './Button.module.scss';
+import { Link } from 'react-router-dom';
 
-function Button({ element, theme, size, text, input, icon = null }) {
+function Button({
+  element = 'button',
+  theme = 'primary',
+  size = 'lg',
+  text,
+  input,
+  icon = null,
+}) {
+  // Element of the button
+  // button and link
+
+  // Theme of the button
+  // primary, secondary-color, secondary-gray, (color, gray, black, white) => only for link
+
+  // Size of the button
+  // sm = font small, padding 8 14
+  // md = font small, padding 10 16
+  // lg = font medium, padding 10 18
+  // xl = font medium, padding 12 20
+  // xxl = font large, padding 16 28
+
   if (element === 'button' && theme === 'primary') {
     return (
       <button {...input} className={`${styles.buttonPrimary} ${styles[size]}`}>
@@ -36,46 +57,70 @@ function Button({ element, theme, size, text, input, icon = null }) {
 
   if (element === 'link' && theme === 'primary') {
     return (
-      <a {...input} className={`${styles.linkPrimary} ${styles[size]}`}>
+      <Link {...input} className={`${styles.linkPrimary} ${styles[size]}`}>
         {icon}
         <span>{text}</span>
-      </a>
+      </Link>
     );
   }
 
   if (element === 'link' && theme === 'secondary-color') {
     return (
-      <a {...input} className={`${styles.linkSecondaryColor} ${styles[size]}`}>
+      <Link
+        {...input}
+        className={`${styles.linkSecondaryColor} ${styles[size]}`}
+      >
         {icon}
         <span>{text}</span>
-      </a>
+      </Link>
     );
   }
 
   if (element === 'link' && theme === 'secondary-gray') {
     return (
-      <a {...input} className={`${styles.linkSecondaryGray} ${styles[size]}`}>
+      <Link
+        {...input}
+        className={`${styles.linkSecondaryGray} ${styles[size]}`}
+      >
         {icon}
         <span>{text}</span>
-      </a>
+      </Link>
     );
   }
 
   if (element === 'link' && theme === 'color') {
     return (
-      <a {...input} className={`${styles.linkColor} ${styles[size]}`}>
+      <Link {...input} className={styles.linkColor}>
         {icon}
         <span>{text}</span>
-      </a>
+      </Link>
     );
   }
 
   if (element === 'link' && theme === 'gray') {
     return (
-      <a {...input} className={`${styles.linkGray} ${styles[size]}`}>
+      <Link {...input} className={styles.linkGray}>
         {icon}
         <span>{text}</span>
-      </a>
+      </Link>
+    );
+  }
+
+  if (element === 'link' && theme === 'black') {
+    return (
+      <Link {...input} className={styles.linkBlack}>
+        {icon}
+        <span>{text}</span>
+      </Link>
+    );
+  }
+
+  if (element === 'link' && theme === 'white') {
+    return (
+      <Link {...input} className={styles.linkWhite}>
+        {icon}
+        <span>{text}</span>
+      </Link>
     );
   }
 }
