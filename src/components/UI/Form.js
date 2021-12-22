9f15a754d9461b753228c169df781f6826389e4c
Saddam Husain <sadamyggakpernahp@gmail.com>
2021-12-22 19:54:31 +0700

    refactor: Change input props in form component so that make more reusable

--- src/components/UI/Form.js
+++ src/components/UI/Form.js
@@ -1,26 +1,24 @@
 import Input from './Input';
 import styles from './Form.module.scss';
 
-function Form({ submitHandler, inputs, children }) {
+function MyForm({ inputs, children, ...props }) {
   const input = inputs?.map(input => (
     <Input
       key={input.id}
       label={input.label}
-      input={{
-        type: input.type,
-        id: input.id,
-        name: input.name,
-        placeholder: input.placeholder,
-        autoComplete: input.autoComplete,
-      }}
+      id={input.id}
+      name={input.name}
+      type={input.type}
+      {...input}
     />
   ));
+
   return (
-    <form onSubmit={submitHandler} className={styles.form}>
+    <form className={styles.form} {...props}>
       {inputs && <div className={styles.field}>{input}</div>}
       {children}
     </form>
   );
 }
 
-export default Form;
+export default MyForm;
