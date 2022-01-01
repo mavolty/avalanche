49e798cc5e5577be144f47fc9ca3203ecf9656b6
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-01-01 22:55:34 +0700

    refactor: Implement redux store and router to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
+import './styles/index.scss';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './styles/index.scss';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
