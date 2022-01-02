ab1d3a58f96a04cec088f15eaad2100e32612ff1
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-01-02 23:24:46 +0700

    feat: Add headless ui dropdown component

---
+++ src/components/UI/Dropdown.js
@@ -0,0 +1,63 @@
+import styles from './Dropdown.module.scss';
+import { Menu } from '@headlessui/react';
+
+function Dropdown({ button, items, account }) {
+  return (
+    <Menu as="div" className={styles.container}>
+      {({ open }) => (
+        <>
+          <Menu.Button className={styles.button}>
+            <span className={styles.buttonText}>{button.text}</span>
+            {button.icon}
+          </Menu.Button>
+          {open && (
+            <div className={styles.dropdown}>
+              {account && (
+                <div className={styles.header}>
+                  {account.avatar}
+                  <div className={styles.headerInfo}>
+                    <p className={styles.headerName}>{account.name}</p>
+                    <p className={styles.headerEmail}>{account.email}</p>
+                  </div>
+                </div>
+              )}
+              <Menu.Items as="ul" static className={styles.items}>
+                {items.map(item => (
+                  <Menu.Item as="li" key={item.text}>
+                    {({ active }) => {
+                      if (item.element === 'link')
+                        return (
+                          <a
+                            className={`${active ? styles.itemActive : ''} ${
+                              styles.item
+                            }`}
+                            href={item.target}
+                          >
+                            {item.text}
+                          </a>
+                        );
+
+                      if (item.element === 'button')
+                        return (
+                          <button
+                            className={`${active ? styles.itemActive : ''} ${
+                              styles.item
+                            }`}
+                            {...item}
+                          >
+                            {item.text}
+                          </button>
+                        );
+                    }}
+                  </Menu.Item>
+                ))}
+              </Menu.Items>
+            </div>
+          )}
+        </>
+      )}
+    </Menu>
+  );
+}
+
+export default Dropdown;
