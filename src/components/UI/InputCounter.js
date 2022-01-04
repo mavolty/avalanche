57326427686f6e34e2888d0d7d9d880b11a08fb6
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-01-04 22:57:11 +0700

    refactor: Change minus and plus logic from inside component to outside using props

--- src/components/UI/InputCounter.js
+++ src/components/UI/InputCounter.js
@@ -1,38 +1,19 @@
 import styles from './InputCounter.module.scss';
-import Icon from './Icons/Icon';
-import { useState } from 'react';
+import Icon from '../Icons/Icon';
 
-function InputCounter({ ...props }) {
-  const [quantity, setQuantity] = useState(1);
-
-  const decrementHandler = () => {
-    if (quantity > 1) {
-      setQuantity(quantity - 1);
-    }
-  };
-
-  const incrementHandler = () => {
-    setQuantity(quantity + 1);
-  };
-
-  const quantityHandler = event => {
-    const value = event.target.value;
-    isFinite(value) && setQuantity(+value);
-  };
-
+function InputCounter({ onMinusClick, onPlusClick, onInputChange, value }) {
   return (
     <div className={styles.container}>
-      <Icon name="minus" color="gray" onClick={decrementHandler} />
+      <Icon name='minus' color='gray' onClick={onMinusClick} />
       <input
-        type="text"
-        id="quantity"
-        name="quantity"
-        step="1"
-        value={quantity}
-        onChange={quantityHandler}
-        {...props}
+        type='text'
+        id='quantity'
+        name='quantity'
+        step='1'
+        value={value}
+        onChange={onInputChange}
       />
-      <Icon name="add" color="gray" onClick={incrementHandler} />
+      <Icon name='add' color='gray' onClick={onPlusClick} />
     </div>
   );
 }
