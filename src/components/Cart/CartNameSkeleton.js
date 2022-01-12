365ba9806efef865082d1bd1035b5ebb23a24b98
Saddam Husain <sadamyggakpernahp@gmail.com>
2022-01-12 17:02:02 +0700

    feat: Add CartNameSkeleton component for the cart page

---
+++ src/components/Cart/CartNameSkeleton.js
@@ -0,0 +1,19 @@
+import ContentLoader from 'react-content-loader';
+
+function CartNameSkeleton(props) {
+  return (
+    <ContentLoader
+      speed={2}
+      width='100%'
+      height='18'
+      viewBox='0 0 325 18'
+      backgroundColor='#f3f3f3'
+      foregroundColor='#ecebeb'
+      {...props}
+    >
+      <rect x='0' y='0' rx='8' ry='8' width='100%' height='18' />
+    </ContentLoader>
+  );
+}
+
+export default CartNameSkeleton;
